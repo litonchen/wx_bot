b0b3ad76cd1039b048ec5d6dc6a8a6ffe08ec3d6
WanStock <33352900+litonchen@users.noreply.github.com>
2023-04-25 21:14:31 +0800

    Update qywx.py

--- qywx.py
+++ qywx.py
@@ -12,9 +12,7 @@ def sent_work_msg(webhook_url,content ):
 def sent_work_markdown(webhook_url,mkd_msg):
     mkd={
         "msgtype": "markdown",
-        "markdown": {
-            "content": mkd_msg
-            }
+        "markdown":  mkd_msg
         }
     body = json.dumps(mkd)
     res = requests.post(webhook_url,data=body)
@@ -39,3 +37,53 @@ def sent_work_img(webhook_url,img):
             }    
     body = json.dumps(mkt)
     res = requests.post(webhook_url,data=body)
+    
+###############    企业微信：机器人      ###############     
+def get_token(corpid, secret):
+    Url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken"
+    Data = {
+      "corpid": corpid,
+      "corpsecret": secret
+    }
+    r = requests.get(url=Url, params=Data)
+    token = r.json()['access_token']
+    return token
+
+def sendImg(access_token,agentid,media_id):
+    #print('media_id:',media_id)
+    data = {
+           "touser": "@all", 
+           "toparty": "@all",         
+           "msgtype" : "image",
+           "agentid" : agentid,
+           "image": {
+               "media_id": media_id
+           },
+           "safe":0
+        }
+    r = requests.post("https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={}".format(access_token), data=json.dumps(data))
+
+def send_txt(access_token,agentid,meg_txt) :
+    data = {
+           "touser": "@all", 
+           "toparty": "@all",         
+           "agentid" : agentid,
+           "msgtype": "text",  # 消息类型，此时固定为text
+           "text": {
+               "content": meg_txt,  # 文本内容，最长不超过2048个字节，必须是utf8编码
+                }
+    }
+    r = requests.post("https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={}".format(access_token), data=json.dumps(data))
+
+def send_markdown(access_token,agentid,mkd_msg) :  
+    send_data = {
+           "touser": "@all", 
+           "toparty": "@all",         
+           "agentid" : agentid,        
+           "msgtype": "markdown",  # 消息类型，此时固定为markdown
+           "markdown": {
+                       "content": mkd_msg
+                   }
+               }
+    r = requests.post("https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={}".format(access_token), data=json.dumps(send_data))
+    # log.info(('markdown：%s'%send_data))
